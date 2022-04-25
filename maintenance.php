428d98942354cdb4b9573ca229076a95e152d208
Giovanni <g.salmeri@mondodomani.org>
2022-04-25 17:37:18 +0200

    Maintenance page description

--- maintenance.php
+++ maintenance.php
@@ -5,18 +5,17 @@ class YellowMaintenance {
     const VERSION = "0.8.19";
     public $yellow;         // access to API
 
-    var $isMaintenanceIp;
-
     // Handle initialisation
     public function onLoad($yellow) {
         $this->yellow = $yellow;
         $this->yellow->system->setDefault("maintenanceIps", "");
-	$this->isMaintenanceIp = in_array($this->yellow->toolbox->getServer("REMOTE_ADDR"), array_merge(preg_split("/\s*,\s*/", $this->yellow->system->get("maintenanceIps")), [ "127.0.0.1", "::1" ]));
     }
 
     // Handle page layout
     public function onParsePageLayout($page, $name) {
-        if (($page->get("status")=="maintenance" || $this->yellow->system->get("status")=="maintenance") && $this->yellow->getRequestHandler()=="core" && !$this->isMaintenanceIp) {
+	$isMaintenanceIp = in_array($this->yellow->toolbox->getServer("REMOTE_ADDR"), array_merge(preg_split("/\s*,\s*/", $this->yellow->system->get("maintenanceIps")), [ "127.0.0.1", "::1" ]));
+        if (($page->get("status")=="maintenance" || $this->yellow->system->get("status")=="maintenance") && $this->yellow->getRequestHandler()=="core" && !$isMaintenanceIp) {
+            $page->set("Description", $this->yellow->language->getTextHtml("maintenanceDescription"));
             $pageError = "";
             if ($this->yellow->extension->isExisting("edit")) {
                 $pageError .= str_replace([ "@url", "@ip" ], [ $page->get("pageEditUrl"), $this->yellow->toolbox->getServer("REMOTE_ADDR") ], $this->yellow->language->getText("maintenancePageError"));
