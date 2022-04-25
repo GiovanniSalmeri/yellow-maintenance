9c0ed6c34a431e5f4234c2ffc64edefeefa77df8
Giovanni <g.salmeri@mondodomani.org>
2022-04-25 21:30:51 +0200

    Lowercase setting name

--- maintenance.php
+++ maintenance.php
@@ -15,7 +15,7 @@ class YellowMaintenance {
     public function onParsePageLayout($page, $name) {
 	$isMaintenanceIp = in_array($this->yellow->toolbox->getServer("REMOTE_ADDR"), array_merge(preg_split("/\s*,\s*/", $this->yellow->system->get("maintenanceIps")), [ "127.0.0.1", "::1" ]));
         if (($page->get("status")=="maintenance" || $this->yellow->system->get("status")=="maintenance") && $this->yellow->getRequestHandler()=="core" && !$isMaintenanceIp) {
-            $page->set("Description", $this->yellow->language->getTextHtml("maintenanceDescription"));
+            $page->set("description", $this->yellow->language->getTextHtml("maintenanceDescription"));
             $pageError = "";
             if ($this->yellow->extension->isExisting("edit")) {
                 $pageError .= str_replace([ "@url", "@ip" ], [ $page->get("pageEditUrl"), $this->yellow->toolbox->getServer("REMOTE_ADDR") ], $this->yellow->language->getText("maintenancePageError"));
